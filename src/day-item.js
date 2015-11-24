484e6c9e26b6bd1374ea7944690b9ac4005737cf
Francis Bongiovanni <fbongiovanni29@gmail.com>
2015-11-23 21:12:02 -0500

    init/final

---
+++ src/day-item.js
@@ -0,0 +1,34 @@
+// Import some code we need
+var React = require('react-native');
+var Text = React.Text;
+
+// Creat our component
+var DayItem = React.createClass({
+  render: function() {
+    return <Text style={this.style()}>
+      {this.props.day}
+    </Text>
+  },
+  style: function() {
+    return {
+      color: this.color(),
+      fontSize: this.fontSize(),
+      lineHeight: this.lineHeight()
+    }
+  },
+  color: function() {
+    var opacity = 1 / this.props.daysUntil;
+    return 'rgba(0, 0, 0, '+ opacity + ')';
+  },
+
+  fontSize: function() {
+    return 60 - 6 * this.props.daysUntil;
+  },
+  lineHeight: function() {
+    return 70 - 4 * this.props.daysUntil;
+  }
+});
+
+
+// Make this code available elsewhere
+module.exports = DayItem;
